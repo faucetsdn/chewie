966551e23b4d15e8090a45505b42893e51f9741d
Michael Baird <michaelb_111@hotmail.com>
2018-07-16 10:40:59 +1200

    refractor duplicate methods. Add check statemachine in correct state.

--- test/test_full_state_machine.py
+++ test/test_full_state_machine.py
@@ -4,13 +4,11 @@ import unittest
 
 from netils import build_byte_string
 
-from chewie.eap import EapIdentity, Eap, EapMd5Challenge
+from chewie.eap import EapIdentity, Eap, EapMd5Challenge, EapSuccess
 from chewie.mac_address import MacAddress
 from chewie.message_parser import EapolStartMessage, IdentityMessage
-
 from chewie.eap_state_machine import FullEAPStateMachine
 from chewie.event import EventMessageReceived, EventRadiusMessageReceived
-from chewie.radius_attributes import EAPMessage
 
 
 class FullStateMachineStartTestCase(unittest.TestCase):
@@ -28,68 +26,57 @@ class FullStateMachineStartTestCase(unittest.TestCase):
         # output EAPIdentityRequest on eap_output_q
         message = EapolStartMessage(self.src_mac)
         self.sm.event(EventMessageReceived(message))
+        self.assertEqual(self.sm.currentState, self.sm.IDLE)
 
         self.assertEqual(self.eap_output_queue.qsize(), 1)
-        output = self.eap_output_queue.get()[0]
+        output = self.eap_output_queue.queue[0][0]
         self.assertIsInstance(output, EapIdentity)
         self.assertEqual(self.radius_output_queue.qsize(), 0)
 
     def test_identity_response(self):
+        self.test_eap_start()
         # input EapIdentityResponse
         # output EapIdentityResponse on radius_output_q
-        message = EapolStartMessage(self.src_mac)
-        self.sm.event(EventMessageReceived(message))
-        self.assertEqual(self.eap_output_queue.qsize(), 1)
-
         message = IdentityMessage(self.src_mac, 1, Eap.RESPONSE, "host1user")
         self.sm.event(EventMessageReceived(message))
+        self.assertEqual(self.sm.currentState, self.sm.AAA_IDLE)
 
         self.assertEqual(self.radius_output_queue.qsize(), 1)
-        self.assertIsInstance(self.radius_output_queue.get()[0], IdentityMessage)
+        self.assertIsInstance(self.radius_output_queue.queue[0][0], IdentityMessage)
         self.assertEqual(self.eap_output_queue.qsize(), 1)
 
     def test_access_challenge(self):
-        message = EapolStartMessage(self.src_mac)
-        self.sm.event(EventMessageReceived(message))
-        self.assertEqual(self.eap_output_queue.qsize(), 1)
+        self.test_identity_response()
 
-        message = IdentityMessage(self.src_mac, 1, Eap.RESPONSE, "host1user")
-        self.sm.event(EventMessageReceived(message))
-
-        self.assertEqual(self.radius_output_queue.qsize(), 1)
-        self.assertIsInstance(self.radius_output_queue.get()[0], IdentityMessage)
-        self.assertEqual(self.eap_output_queue.qsize(), 1)
-
-        eap_message = EAPMessage.parse(EapMd5Challenge(Eap.REQUEST, 2, build_byte_string("74d3db089b727d9cc5774599e4a32a29"), b"host1user").pack())
+        eap_message = EapMd5Challenge(Eap.REQUEST, 2, build_byte_string("74d3db089b727d9cc5774599e4a32a29"), b"host1user")
         self.sm.event(EventRadiusMessageReceived(eap_message, None))
+        self.assertEqual(self.sm.currentState, self.sm.IDLE2)
 
         self.assertEqual(self.eap_output_queue.qsize(), 2)
-        self.assertEqual(self.radius_output_queue.qsize(), 0)
+        self.assertIsInstance(self.eap_output_queue.queue[1][0], EapMd5Challenge)
 
+        self.assertEqual(self.radius_output_queue.qsize(), 1)
 
     def test_md5_challenge_response(self):
-        message = EapolStartMessage(self.src_mac)
-        self.sm.event(EventMessageReceived(message))
-        self.assertEqual(self.eap_output_queue.qsize(), 1)
-
-        message = IdentityMessage(self.src_mac, 1, Eap.RESPONSE, "host1user")
-        self.sm.event(EventMessageReceived(message))
-
-        self.assertEqual(self.radius_output_queue.qsize(), 1)
-        self.assertIsInstance(self.radius_output_queue.get()[0], IdentityMessage)
-        self.assertEqual(self.eap_output_queue.qsize(), 1)
-
-        eap_message = EAPMessage.parse(EapMd5Challenge(Eap.REQUEST, 2, build_byte_string("74d3db089b727d9cc5774599e4a32a29"), b"host1user").pack())
-        self.sm.event(EventRadiusMessageReceived(eap_message, None))
-
-        self.assertEqual(self.eap_output_queue.qsize(), 2)
-        self.assertEqual(self.radius_output_queue.qsize(), 0)
+        self.test_access_challenge()
 
         message = EapMd5Challenge(Eap.RESPONSE, 2, build_byte_string("3a535f0ee8c6b34fe714aa7dad9a0e15"), b"host1user")
         self.sm.event(EventMessageReceived(message))
-
+        self.assertEqual(self.sm.currentState, self.sm.AAA_IDLE)
         self.assertEqual(self.eap_output_queue.qsize(), 2)
-        self.assertEqual(self.radius_output_queue.qsize(), 1)
+        self.assertEqual(self.radius_output_queue.qsize(), 2)
+        self.assertIsInstance(self.radius_output_queue.queue[1][0], EapMd5Challenge)
+
+    def test_access_accept(self):
+        self.test_md5_challenge_response()
+
+        message = EapSuccess(3)
+        self.sm.event(EventRadiusMessageReceived(message, None))
+
+        self.assertEqual(self.sm.currentState, self.sm.SUCCESS2)
+        self.assertEqual(self.eap_output_queue.qsize(), 3)
+        self.assertIsInstance(self.eap_output_queue.queue[2][0], EapSuccess)
+        self.assertEqual(self.radius_output_queue.qsize(), 2)
 
     def MD5ChallengeEapTest(self):
         pass
