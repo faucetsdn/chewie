a74c68f09ff008517124f98520ad755218cb0c95
Josh Bailey <joshb@google.com>
2018-09-03 18:09:34 +1200

    Revert "0.0.8 and add python3.[45] to CI."
    This reverts commit 8ef0fbabd144559a95af23607d6a82a26d0e3cc8.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='chewie',
     description='A bare-bones EAPOL/802.1x implementation',
     long_description=long_description,
-    version='0.0.8',
+    version='0.0.7',
     url='https://github.com/faucetsdn/chewie',
     author='Sam Russell',
     author_email='sam.h.russell@gmail.com',
