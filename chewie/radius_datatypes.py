40047d1ce82640b3bf40cd71aa5a5b6cf80eb339
Michael Baird <michaelb_111@hotmail.com>
2018-09-04 10:06:33 +1200

    attempt 2 to fix python3.4 import error

--- chewie/radius_datatypes.py
+++ chewie/radius_datatypes.py
@@ -3,7 +3,7 @@ import abc
 import math
 import struct
 
-from chewie.message_parser import EapMessage, MessagePacker, MessageParser
+import chewie.message_parser
 
 
 class DataType(object):
@@ -175,8 +175,8 @@ class Concat(DataType):
 
     def __init__(self, bytes_data=None, raw_data=None):
         if raw_data:
-            if isinstance(raw_data, EapMessage):
-                bytes_data = MessagePacker.eap_pack(raw_data)[2]
+            if isinstance(raw_data, chewie.message_parser.EapMessage):
+                bytes_data = chewie.message_parser.MessagePacker.eap_pack(raw_data)[2]
             else:
                 bytes_data = bytes.fromhex(raw_data)
             # self.is_valid_length(data)
@@ -212,7 +212,7 @@ class Concat(DataType):
         return packed
 
     def data(self):
-        return MessageParser.eap_parse(self.bytes_data, None)
+        return chewie.message_parser.MessageParser.eap_parse(self.bytes_data, None)
 
     def full_length(self):
         return self.AVP_HEADER_LEN * \
