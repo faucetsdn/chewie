cf7927299b2ac7b3f14455d5ffbcbe785edcdc8c
Michael Baird <michaelb_111@hotmail.com>
2018-09-04 14:25:44 +1200

    detect travis python version

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,10 @@
 # TODO: must be run from chewie root
 # TODO: add pylint
 pip install .
-PYTHONPATH=./ pytest --cov=chewie --cov-report term --cov-report=xml:coverage.xml test/test_*.py && pytype -V$TRAVIS_PYTHON_VERSION chewie/*py
+if [ -z "${TRAVIS_PYTHON_VERSION}" ]; then
+    PYTYPE_TARGET_VERSION=3.6
+else
+    PYTYPE_TARGET_VERSION=$TRAVIS_PYTHON_VERSION
+fi
+
+PYTHONPATH=./ pytest --cov=chewie --cov-report term --cov-report=xml:coverage.xml test/test_*.py && pytype -V$PYTYPE_TARGET_VERSION chewie/*py
