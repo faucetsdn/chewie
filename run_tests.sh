ba767742f1ecc8380f8417a3f5a0fb122cc55bf2
Josh Bailey <joshb@google.com>
2018-09-04 12:23:33 +1200

    Disable pytype.

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,6 @@
 
 # TODO: must be run from chewie root
 # TODO: add pylint
-PYTHONPATH=./ pytest --cov=chewie --cov-report=xml:coverage.xml test/ && pytype -V3.5 -d import-error chewie/*py
+PYTHONPATH=./ pytest --cov=chewie --cov-report=xml:coverage.xml test/ 
+# TODO: add pytype
+# && pytype -V3.5 -d import-error chewie/*py
