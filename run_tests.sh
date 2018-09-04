f3bfee37fdfec715a76aba547d7036d81b912e23
Michael Baird <michaelb_111@hotmail.com>
2018-09-04 13:59:06 +1200

    install

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # TODO: must be run from chewie root
 # TODO: add pylint
+pip install .
 PYTHONPATH=./ pytest --cov=chewie --cov-report term --cov-report=xml:coverage.xml test/test_*.py && pytype -V$TRAVIS_PYTHON_VERSION chewie/*py
