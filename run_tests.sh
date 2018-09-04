8987b9f3429e92525ac00463bd88d2e609a069b9
Michael Baird <michaelb_111@hotmail.com>
2018-09-04 13:45:07 +1200

    merge

--- run_tests.sh
+++ run_tests.sh
@@ -2,6 +2,6 @@
 
 # TODO: must be run from chewie root
 # TODO: add pylint
-PYTHONPATH=./ pytest --cov=chewie --cov-report=xml:coverage.xml test/ 
+PYTHONPATH=./ pytest --cov=chewie --cov-report=xml:coverage.xml test/
 # TODO: add pytype
 # && pytype -V3.5 -d import-error chewie/*py
