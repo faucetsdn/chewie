f117e00ef9084c95531d6abbf887e194da57c676
Michael Baird <michaelb_111@hotmail.com>
2018-09-26 11:07:40 +1200

    pylint

--- chewie/timer_scheduler.py
+++ chewie/timer_scheduler.py
@@ -1,3 +1,4 @@
+"""Homebrew Event scheduler, as sched.scheduler was not working outside of unittests"""
 import heapq
 import time
 
@@ -5,6 +6,7 @@ import eventlet
 
 
 class TimerScheduler:
+    """wraps a heapbased queue with a similar api to asyncio.loop"""
 
     def __init__(self, logger, sleep=None):
         self.logger = logger
@@ -15,6 +17,16 @@ class TimerScheduler:
             self.sleep = sleep
 
     def call_later(self, timeout, func, *args):
+        """
+
+        Args:
+            timeout: number of seconds to delay executing func
+            func: function to execute
+            *args: arguments for func
+
+        Returns:
+            tuple (expiry_time, dict) - can be used for cancelling the job
+        """
         if not args:
             args = []
         job = (time.time() + timeout, {'func': func, 'args': args, 'alive': True})
@@ -22,6 +34,7 @@ class TimerScheduler:
         return job
 
     def run(self):
+        """Main loop. should run forever"""
         try:
             while True:
                 if len(self.timer_heap):
@@ -33,10 +46,8 @@ class TimerScheduler:
                         else:
                             self.logger.info('job %s has been cancelled', job['func'].__name__)
                     else:
-                        self.logger.info('too early for job - sleeping')
                         self.sleep(1)
                 else:
-                    self.logger.info('job Queue empty - sleeping')
                     self.sleep(1)
         except Exception as e:
             self.logger.exception(e)
