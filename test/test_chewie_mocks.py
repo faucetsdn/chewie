c669bc96f2ef8a93a7eb8f8f249439f3ed95c890
Sam Russell <sam.h.russell@gmail.com>
2018-11-16 21:48:33 +1300

    Rework mocks, two methods tested now

--- test/test_chewie_mocks.py
+++ test/test_chewie_mocks.py
@@ -5,25 +5,18 @@ from collections import namedtuple
 import unittest
 from unittest.mock import patch, Mock
 
-from eventlet.queue import Queue
-
 from chewie.chewie import Chewie
 from chewie.event import EventMessageReceived
 
-class FakeSocket:
-    """Helper for socket wrappers"""
+def return_if(expected, return_value):
+    """allows us to do expect-this-return-that style mocking"""
+    def inner_function(*args):
+        """workaround to effectively give us an anonymous function"""
+        if args == expected:
+            return return_value
+        raise Exception("Expected %s but got %s" % (expected, args))
 
-    def __init__(self):
-        self.receive_queue = Queue()
-        self.send_queue = Queue()
-
-    def receive(self):
-        """Fake receive method"""
-        return self.receive_queue.get()
-
-    def send(self, data):
-        """Fake receive method"""
-        self.send_queue.put(data)
+    return inner_function
 
 FakeLogger = namedtuple('FakeLogger', ('name',)) # pylint: disable=invalid-name
 FakeMessage = namedtuple('FakeMessage', ('src_mac',)) # pylint: disable=invalid-name
@@ -37,17 +30,29 @@ class ChewieWithMocksTestCase(unittest.TestCase):
                              '127.0.0.1', 1812, 'SECRET',
                              '44:44:44:44:44:44')
 
-    @patch("chewie.chewie.FullEAPStateMachine")
     @patch("chewie.chewie.Chewie.running", Mock(side_effect=[True, False]))
-    @patch("chewie.chewie.MessageParser.ethernet_parse",
-           Mock(return_value=[FakeMessage('fake src mac'), 'fake dst mac'])
-          )
+    @patch("chewie.chewie.MessageParser.ethernet_parse")
+    @patch("chewie.chewie.FullEAPStateMachine")
     @patch("chewie.chewie.sleep", Mock())
-    def test_eap_packet_goes_to_new_state_machine(self, state_machine):
+    def test_eap_packet_in_goes_to_new_state_machine(self, state_machine, ethernet_parse): #pylint: disable=invalid-name
         """test EAP packet creates a new state machine and is sent on"""
-        self.chewie.eap_socket = FakeSocket()
-        self.chewie.eap_socket.receive_queue.put("input eap message")
+        self.chewie.eap_socket = Mock(**{'receive.return_value': 'message from socket'})
+        ethernet_parse.side_effect = return_if(
+            ('message from socket',),
+            (FakeMessage('fake src mac'), 'fake dst mac')
+            )
         self.chewie.receive_eap_messages()
         state_machine().event.assert_called_with(
             EventMessageReceived(FakeMessage('fake src mac'), 'fake dst mac')
             )
+
+    @patch("chewie.chewie.Chewie.running", Mock(side_effect=[True, False]))
+    @patch("chewie.chewie.MessagePacker.ethernet_pack")
+    @patch("chewie.chewie.sleep", Mock())
+    def test_eap_output_packet_gets_packed_and_sent(self, ethernet_pack): #pylint: disable=invalid-name
+        """test EAP packet creates a new state machine and is sent on"""
+        self.chewie.eap_socket = Mock()
+        ethernet_pack.return_value = "packed ethernet"
+        self.chewie.eap_output_messages.put(["output eap message", "src mac", "port mac"])
+        self.chewie.send_eap_messages()
+        self.chewie.eap_socket.send.assert_called_with("packed ethernet")
