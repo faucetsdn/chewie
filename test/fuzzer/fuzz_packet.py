f08d44c6111cef8043876d5cd98610fe7851a6de
Michael Baird <michaelb_111@hotmail.com>
2018-11-19 16:24:29 +1300

    eap fuzzer working

--- test/fuzzer/fuzz_packet.py
+++ test/fuzzer/fuzz_packet.py
@@ -2,57 +2,66 @@
 
 """Run AFL repeatedly with externally supplied generated packet from STDIN."""
 
-import logging
 import struct
 import sys
 
 import afl
 
 #from chewie.radius import InvalidResponseAuthenticatorError, InvalidMessageAuthenticatorError
+from chewie.mac_address import MacAddress
 from chewie.message_parser import MessageParser
 
 ROUNDS = 1
-logging.disable(logging.CRITICAL)
 
 
 def main(eap):
     """Run AFL repeatedly with externally supplied generated packet from STDIN."""
 
     while afl.loop(ROUNDS):
+    # afl.start()
+    # if True:
         # receive input from afl
         rcv = sys.stdin.read()
         data = None
         try:
-            data = bytearray.fromhex(rcv) # pytype: disable=missing-parameter
+            data = bytearray.fromhex(rcv)  # pytype: disable=missing-parameter
         except (ValueError, TypeError):
-            continue
+            return
         if eap:
-            test_ethernet_parse(data)
-        else:
-            test_radius_parse(data)
+            test_eap_parse(data)
+        # else:
+        #     test_radius_parse(data)
+#
+#
+# def test_ethernet_parse(data):
+#     try:
+#         MessageParser.ethernet_parse(data)
+#     except (ValueError, struct.error) as e:
+#         if e.message.startswith("unpack requires a buffer of 14 bytes"):
+#             pass
+#         elif e.message.startswith("Ethernet packet with bad ethertype received:"):
+#             pass
+#         else:
+#             raise
 
 
-def test_ethernet_parse(data):
+def test_eap_parse(data):
     try:
-        MessageParser.ethernet_parse(data)
-    except (ValueError, struct.error) as e:
-        if e.message.startswith("unpack requires a buffer of 14 bytes"):
-            pass
-        elif e.message.startswith("Ethernet packet with bad ethertype received:"):
-            pass
-        else:
-            raise
+        MessageParser.eap_parse(data, MacAddress.from_string("00:00:00:12:34:56"))
+    except (KeyError, UnicodeDecodeError, ValueError, struct.error) as e:
+        # Ignore exceptions the parser intentionally throws, and are caught by the caller.
+        pass
 
 
-def test_radius_parse(data):
-    try:
-        MessageParser.radius_parse(data, "SECRET", request_authenticator_callback=lambda x: None)
-    # except (InvalidResponseAuthenticatorError, InvalidMessageAuthenticatorError) as e:
-    except (ValueError) as e:
-        if e.message.startswith("Unable to parse radius code:"):
-            pass
-        else:
-            raise
+# def test_radius_parse(data):
+#     try:
+#         MessageParser.radius_parse(data, "SECRET", request_authenticator_callback=lambda x: None)
+#     # except (InvalidResponseAuthenticatorError, InvalidMessageAuthenticatorError) as e:
+#     except (ValueError) as e:
+#         if e.message.startswith("Unable to parse radius code:"):
+#             pass
+#         else:
+#             raise
 
 
 if __name__ == "__main__":
