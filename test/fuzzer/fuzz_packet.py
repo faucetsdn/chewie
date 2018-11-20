86f7c65b58304dcf8650ed1439ba3320062acd0a
Michael Baird <michaelb_111@hotmail.com>
2018-11-20 15:31:20 +1300

    fuzz radius

--- test/fuzzer/fuzz_packet.py
+++ test/fuzzer/fuzz_packet.py
@@ -2,14 +2,17 @@
 
 """Run AFL repeatedly with externally supplied generated packet from STDIN."""
 
-import struct
+
 import sys
+from collections import namedtuple
 
 import afl
 
-#from chewie.radius import InvalidResponseAuthenticatorError, InvalidMessageAuthenticatorError
+from chewie.utils import MessageParseError
+
 from chewie.mac_address import MacAddress
 from chewie.message_parser import MessageParser
+from chewie.radius import InvalidResponseAuthenticatorError, InvalidMessageAuthenticatorError
 
 ROUNDS = 1
 
@@ -29,8 +32,8 @@ def main(eap):
             return
         if eap:
             test_eap_parse(data)
-        # else:
-        #     test_radius_parse(data)
+        else:
+            test_radius_parse(data)
 #
 #
 # def test_ethernet_parse(data):
@@ -48,20 +51,26 @@ def main(eap):
 def test_eap_parse(data):
     try:
         MessageParser.eap_parse(data, MacAddress.from_string("00:00:00:12:34:56"))
-    except (KeyError, UnicodeDecodeError, ValueError, struct.error) as e:
+    except MessageParseError as e:
         # Ignore exceptions the parser intentionally throws, and are caught by the caller.
         pass
 
 
-# def test_radius_parse(data):
-#     try:
-#         MessageParser.radius_parse(data, "SECRET", request_authenticator_callback=lambda x: None)
-#     # except (InvalidResponseAuthenticatorError, InvalidMessageAuthenticatorError) as e:
-#     except (ValueError) as e:
-#         if e.message.startswith("Unable to parse radius code:"):
-#             pass
-#         else:
-#             raise
+def test_radius_parse(data):
+    try:
+        # The dict sets the packet ID for the known packet_id (test/fuzzer/radius_packet*.ex)
+        #  to none. so no validation is done.
+        MessageParser.radius_parse(data, "SECRET",
+                                   radius_lifecycle=namedtuple('RadiusLifecycle',
+                                                               'packet_id_to_request_authenticator')
+                                   ({0x0f:None, 0x08: None, 0x0a: None, 0xbc: None, }))
+    except MessageParseError as e:
+        pass
+    # except (ValueError) as e:
+    #     if e.message.startswith("Unable to parse radius code:"):
+    #         pass
+    #     else:
+    #         raise
 
 
 if __name__ == "__main__":
