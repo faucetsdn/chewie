312f31adca23db705cbc7241affd8d3fbcb1305f
Michael Baird <michaelb_111@hotmail.com>
2018-12-14 10:57:49 +1300

    use PBR

--- setup.py
+++ setup.py
@@ -1,33 +1,7 @@
 from setuptools import setup
 
-long_description = """
-    Chewie is an EAPOL/802.1x implementation in Python.
-
-    Currently barely works, useful if you have one user and their password is "microphone"
-
-    More information at https://github.com/faucetsdn/chewie
-"""
 
 setup(
-    name='chewie',
-    description='A bare-bones EAPOL/802.1x implementation',
-    long_description=long_description,
-    version='0.0.12',
-    url='https://github.com/faucetsdn/chewie',
-    author='Sam Russell',
-    author_email='sam.h.russell@gmail.com',
-    license='Apache2',
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3'
-    ],
-    keywords='802.1x dot1x eap eapol radius authentication aaa',
-    packages=['chewie'],
-    python_requires='>=3',
-    install_requires=[
-        'eventlet',
-        'transitions'
-    ]
+    setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    pbr=True
 )
