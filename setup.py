f9b1ffa0807fad2f9c24d1d88f8888d61f694f33
Brad Cowie <brad@wand.net.nz>
2019-08-13 14:24:20 +1200

    Require python3 for chewie.

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
+import sys
+
 from setuptools import setup
 
+if sys.version_info < (3,):
+    print("""You are trying to install beka on python {py}
+
+chewie is not compatible with python 2, please upgrade to python 3.5 or newer."""
+          .format(py='.'.join([str(v) for v in sys.version_info[:3]])), file=sys.stderr)
+    sys.exit(1)
 
 setup(
+    name='chewie',
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
+    python_requires='>=3.5',
     pbr=True
 )
