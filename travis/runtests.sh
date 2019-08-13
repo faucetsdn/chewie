af5243eb0b05f462c2d313a6d0fa8fcf123e750b
Brad Cowie <brad@wand.net.nz>
2019-08-13 17:14:30 +1200

    Fix bash errors.

--- travis/runtests.sh
+++ travis/runtests.sh
@@ -5,22 +5,22 @@ set -e  # quit on error
 echo TRAVIS_BRANCH: $TRAVIS_BRANCH
 echo TRAVIS_COMMIT: $TRAVIS_COMMIT
 
-if [ $TRAVIS_SHARD == "unittest" ]; then
+if [ "$TRAVIS_SHARD" == "unittest" ]; then
   env CHEWIE_TESTS="-z" ./run_tests.sh
 
   env CHEWIE_TESTS="-u" ./run_tests.sh
 
-  if [ $CODE_COV == "true" ]; then
+  if [ "$CODE_COV" == "true" ]; then
     codecov || true
   fi
 
-  if [ $CODE_CHECK == "true" ]; then
+  if [ "$CODE_CHECK" == "true" ]; then
     env CHEWIE_TESTS="-n" ./run_tests.sh
   fi
 
   exit 0
 fi
 
-if [ $TRAVIS_SHARD == "integration" ]; then
+if [ "$TRAVIS_SHARD" == "integration" ]; then
   env CHEWIE_TESTS="-i" ./run_tests.sh
 fi
