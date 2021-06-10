6cdc3233a362736bef34d2b8447a7f04046b7fc4
Brad Cowie <brad@wand.net.nz>
2021-06-11 11:47:17 +1200

    Separate pytype from unit tests so they can run on python3.5.

--- docker/pip_deps.sh
+++ docker/pip_deps.sh
@@ -9,6 +9,17 @@ BASEDIR=$(readlink -f "${SCRIPTDIR}/..")
 reqs="test-requirements.txt requirements.txt"
 pip_args=""
 
+for opt in "$@"; do
+  case "${opt}" in
+    --pip-args=*)
+      pip_args="${pip_args} ${opt#*=}"
+      ;;
+    --extra-requirements=*)
+      reqs="${reqs} ${opt#*=}"
+      ;;
+  esac
+done
+
 pip3="pip3 install -q --upgrade ${pip_args}"
 
 "${BASEDIR}/docker/retrycmd.sh" "${pip3} wheel"
