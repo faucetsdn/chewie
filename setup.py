5a60f5845ec7e9b75036677210deff4346720727
Brad Cowie <brad@faucet.nz>
2023-01-18 09:54:42 +1300

    Drop support for python 3.7.

--- setup.py
+++ setup.py
@@ -9,13 +9,13 @@ from setuptools import setup
 if sys.version_info < (3,):
     print("""You are trying to install chewie on python {py}
 
-chewie is not compatible with python 2, please upgrade to python 3.7 or newer."""
+chewie is not compatible with python 2, please upgrade to python 3.8 or newer."""
           .format(py='.'.join([str(v) for v in sys.version_info[:3]])), file=sys.stderr)
     sys.exit(1)
 
 setup(
     name='chewie',
     setup_requires=['pbr>=1.9', 'setuptools>=17.1'],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     pbr=True
 )
